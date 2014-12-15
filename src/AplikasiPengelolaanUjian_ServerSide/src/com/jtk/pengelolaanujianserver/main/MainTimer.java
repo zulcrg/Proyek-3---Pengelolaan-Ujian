1a12c05df7663c0e0f33c2063d86510dfb8c65aa
eripahle <eripahle@gmail.com>
2014-12-15 10:56:03 +0700

    perubahan struktur reminder final

--- src/AplikasiPengelolaanUjian_ServerSide/src/com/jtk/pengelolaanujianserver/main/MainTimer.java
+++ src/AplikasiPengelolaanUjian_ServerSide/src/com/jtk/pengelolaanujianserver/main/MainTimer.java
@@ -3,17 +3,18 @@ package com.jtk.pengelolaanujianserver.main;
 
 
 import com.jtk.pengelolaanujian.controller.admin.RegistrasiUserController;
+import com.jtk.pengelolaanujianserver.proces.SendingSMSControlProcess;
 import com.jtk.pengelolaanujianserver.proces.ValidityActionControlProcess;
 import java.util.Timer;
  
-public class TimerSample {
+public class MainTimer {
  
     public static void main(String[] args) {
         //1- Taking an instance of Timer class.
         Timer timer = new Timer();
  
         //2- Taking an instance of class contains your repeated method.        
-        ValidityActionControlProcess actionControl = new ValidityActionControlProcess();
+        SendingSMSControlProcess actionControl = new SendingSMSControlProcess();
  
         //TimerTask is a class implements Runnable interface so
         //You have to override run method with your certain code black
