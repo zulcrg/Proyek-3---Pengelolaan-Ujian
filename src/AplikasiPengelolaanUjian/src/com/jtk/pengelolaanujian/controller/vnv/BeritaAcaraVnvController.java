24ae9da1aeff204acc17c21c913b7de6f3a944f5
cluvalentine <cluvalentine@gmail.com>
2015-01-04 01:33:44 +0700

    Berita acara vnv dan query

--- src/AplikasiPengelolaanUjian/src/com/jtk/pengelolaanujian/controller/vnv/BeritaAcaraVnvController.java
+++ src/AplikasiPengelolaanUjian/src/com/jtk/pengelolaanujian/controller/vnv/BeritaAcaraVnvController.java
@@ -6,10 +6,40 @@
 
 package com.jtk.pengelolaanujian.controller.vnv;
 
+import com.jtk.pengelolaanujian.entity.Soal;
+import com.jtk.pengelolaanujian.facade.SoalFacade;
+import java.util.List;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author Rizki
  */
 public class BeritaAcaraVnvController {
-    
+    public void searchSoal(String text, JTable tSoal){
+        List<Soal> soalList;
+        
+        SoalFacade soalFacade = new SoalFacade();
+        soalList = soalFacade.searchSoalInKbk(text);
+
+        Object[] columnsName = {"Kode Soal", "Mata Kuliah", "Mata Kuliah Tipe"};
+
+        DefaultTableModel dtm = new DefaultTableModel(null, columnsName) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+        for (Soal soal : soalList) {
+            Object[] o = new Object[3];
+            o[0] = soal.getSoalKode();
+            o[1] = soal.getMataKuliah();
+            o[2] = soal.getMatkulTipe();
+            
+
+            dtm.addRow(o);
+        }
+        tSoal.setModel(dtm);
+    }
 }
