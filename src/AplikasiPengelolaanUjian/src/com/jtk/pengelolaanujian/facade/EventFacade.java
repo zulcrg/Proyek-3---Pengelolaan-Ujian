34e4798a18865c973544394fb8653eaca1dcbd0e
eripahle <eripahle@gmail.com>
2015-01-06 03:07:02 +0700

    perubahan dikit di create event

--- src/AplikasiPengelolaanUjian/src/com/jtk/pengelolaanujian/facade/EventFacade.java
+++ src/AplikasiPengelolaanUjian/src/com/jtk/pengelolaanujian/facade/EventFacade.java
@@ -142,11 +142,11 @@ public class EventFacade {
                 event.setUploadSelesai(rs.getDate(5));
                 event.setVnvMulai(rs.getDate(6));
                 event.setVnvSelesai(rs.getDate(7));
-                event.setDelayUploadSoal(rs.getInt(8));
-                event.setDelayPengawas(rs.getInt(9));
-                event.setDelayUploadNilai(rs.getInt(10));
-                event.setUploadNilaiSelesai(rs.getDate(11));
-                event.setTimeReminder(rs.getInt(12));
+                event.setUploadNilaiSelesai(rs.getDate(8));
+                event.setDelayUploadSoal(rs.getInt(10));
+                event.setDelayPengawas(rs.getInt(11));
+                event.setDelayUploadNilai(rs.getInt(12));                
+                event.setTimeReminder(rs.getInt(13));
                 return event;
             }
         } catch (SQLException ex) {
